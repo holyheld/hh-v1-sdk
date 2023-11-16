644fa48041fe983756c5182fb9ababeeb0d80c0f
johnelectron8 <johnelectron8@gmail.com>
2023-11-16 11:17:00 +0400

    [Feature] Update shared library

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,5 @@
 import { WalletClient, Transport, Chain, Account } from 'viem';
 import type {
-  GetWalletClientArgs,
   WalletClientAdapter,
   WalletClientExecutor,
 } from '@holyheld/web-app-shared/sdklib/bundle';
@@ -8,10 +7,10 @@ import { HHError } from '@holyheld/web-app-shared/sdklib/bundle';
 
 export const createWalletClientAdapter = (client: WalletClient | null): WalletClientAdapter => {
   return {
-    useWalletClient(args?: GetWalletClientArgs): WalletClientExecutor {
+    useWalletClient(): WalletClientExecutor {
       return async (executor) => {
         if (client === null) {
-          throw new HHError('Empty wallet client received', { payload: args });
+          throw new HHError('Empty wallet client received');
         }
 
         return executor(client as WalletClient<Transport, Chain, Account>);
